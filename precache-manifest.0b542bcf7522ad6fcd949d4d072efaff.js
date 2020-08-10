d715b355d071847929c8d6dba2f8b72e0c36e4e7
sumant kumar <sumantkumar735568@gmail.com>
2020-08-10 23:47:50 +0530

    Updates

---
+++ precache-manifest.0b542bcf7522ad6fcd949d4d072efaff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84bd5a8d38c7546efbd3bba62ce274c6",
+    "url": "/react-git-in-demo/index.html"
+  },
+  {
+    "revision": "3c1a4b3548d7e24e07ee",
+    "url": "/react-git-in-demo/static/css/main.0335f34c.chunk.css"
+  },
+  {
+    "revision": "2ef0323c5c36f79cd41e",
+    "url": "/react-git-in-demo/static/js/2.cf3a152d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-git-in-demo/static/js/2.cf3a152d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c1a4b3548d7e24e07ee",
+    "url": "/react-git-in-demo/static/js/main.e5c7eedd.chunk.js"
+  },
+  {
+    "revision": "ac7e0c3f435ed51ce383",
+    "url": "/react-git-in-demo/static/js/runtime-main.16702831.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-git-in-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
